2187ef8f533ed69b7c3d0b133106bebc5475bcbc
Pranav313 <pranav.kulkarni@xpressbees.com>
2021-04-21 19:50:42 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,100 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+      
+
+      <div class="container border border-dark shadow-lg p-3 mb-5 bg-white rounded" style="margin-top:10px; background-color:rgb(85, 91, 97)!important">
+        <div class="form-group row" style="margin-top:10px;">
+          <div class="col-sm-2">
+            <select name="method" v-model="method" v-bind:class="{'form-control': true}">
+              <option value="get">GET</option>
+              <option value="post">POST</option>
+            </select>
+          </div>
+          <div class="col-sm-8">
+            <input type="text" class="form-control" v-model="url" placeholder="Enter URL here">
+          </div>
+          <div class="col-sm-2">
+            <button type="submit" class="form-control btn btn-primary" style="background-color:var(--accent-color)" @click="validateJson">SEND</button>
+          </div>
+          <div class="col-sm-2" style="margin-top:10px;">
+            <label class="form-control">Header</label>
+          </div>
+          <div class="col-sm-5" style="margin-top:10px;">
+            <label class="form-control">Content-type</label>
+          </div>
+          <div class="col-sm-5" style="margin-top:10px;">
+            <!-- <label class="form-control">Application/JSON</label> -->
+            <select name="c_type" v-model="c_type" v-bind:class="{'form-control': true}">
+              <option value="application/json">application/json</option>
+              <option value="multipart/form-data">multipart/form-data</option>
+            </select>
+          </div>
+          <div class="col-sm-12" style="margin-top:10px;" v-show="c_type=='multipart/form-data'">
+            <input type="file" id="fileinput">
+          </div>
+          <div class="col-sm-6" style="margin-top:10px; border-radius:0rem !important">
+            <label class="form-control">Payload</label>
+          </div>
+          <div class="col-sm-6" style="margin-top:10px;">
+            <label class="form-control">Response</label>
+          </div>
+          <div class="col-sm-6" style="margin-top:-8px;">
+            <textarea name="jsonpayload" v-model="jsonpayload" cols="3" rows="21" class="form-control" ></textarea>
+          </div>
+          <div class="col-sm-6" style="margin-top:-8px;">
+            <textarea name="response" v-model="response" cols="3" rows="21" class="form-control" disabled></textarea>
+          </div>
+        </div>
+        
+          
+
+      </div>
+
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      url : '',
+      method : 'post',
+      jsonpayload : '',
+      response : '',
+      c_type : 'application/json'
+    }
+  },
+  methods : {
+    validateJson(){
+      try{
+        if (this.jsonpayload) {
+        JSON.parse(this.jsonpayload)  
+        }
+        
+      }
+      catch(exception){
+        alert('enter valid JSON')
+        return false;
+      }
+      axios({
+        method : this.method,
+        url : this.url,
+        data : (this.jsonpayload) ? JSON.parse(this.jsonpayload) : {}
+      }).then((result)=>{
+        console.log(result.data);
+        
+        this.response = JSON.stringify(result.data, null, "\t")
+      })
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -54,5 +109,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.form-control{
+  background-color:#e9ecef 
 }
 </style>
